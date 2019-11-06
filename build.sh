f2f45fd26901b672ea368f0be298a1422ac0f1cb
Coby Tamayo <acobster@gmail.com>
2019-11-05 22:25:25 -0800

    run file-based builds

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Download and install Racket
+curl -sSo /tmp/install-racket https://mirror.racket-lang.org/installers/7.4/racket-7.4-x86_64-linux.sh
+/bin/sh /tmp/install-racket --in-place --dest /tmp/racket
+
+# Install Pollen lang
+/tmp/racket/bin/raco pkg install --auto pollen
+
+# Render the static assets
+/tmp/racket/bin/raco pollen render
