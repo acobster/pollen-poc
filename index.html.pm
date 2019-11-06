bcd9383490bdf54833763e275419a857709c4612
Coby Tamayo <acobster@gmail.com>
2019-11-05 23:22:39 -0800

    more boilerplate 👷‍♀️

--- index.html.pm
+++ index.html.pm
@@ -2,7 +2,16 @@
 
 ◊h1{Hello, World!}
 
+Some "stuff"
+Line break's are great
+
+Second P
+
 ◊ul{
   ◊item{You ◊strong{wanted} it — you ◊em{got} it.}
   ◊item{◊link["https://example.com"]{Example Link}}
+}
+
+◊code["clj"]{
+  hello
 }
--- pollen.rkt
+++ pollen.rkt
@@ -1,7 +1,18 @@
 #lang racket/base
-(require pollen/tag)
+(require pollen/tag pollen/decode txexpr)
 (provide (all-defined-out))
+
+(define (root . elements)
+  (txexpr 'main
+          empty
+          (decode-elements
+            elements
+            #:txexpr-elements-proc decode-paragraphs
+            #:string-proc (compose1 smart-quotes smart-dashes))))
 
 (define items (default-tag-function 'ul))
 (define item (default-tag-function 'li 'p))
 (define (link url text) `(a ((href ,url)) ,text))
+
+(define (code lang snippet) `(pre ((class ,(string-append "lang--" lang)))
+                                  (code ,snippet)))
