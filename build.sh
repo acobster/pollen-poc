32aeb388491c14cfa1214d8ce577900a41a08a81
Coby Tamayo <acobster@gmail.com>
2019-11-06 09:39:33 -0800

    render homepage explicitly 🤔

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ curl -sSo /tmp/install-racket https://mirror.racket-lang.org/installers/7.4/rack
 # Install Pollen lang
 /tmp/racket/bin/raco pkg install --auto pollen
 
-# Render the static assets
+# Render the static pages
 /tmp/racket/bin/raco pollen render
+# Render homepage explicitly for now:
+/tmp/racket/bin/raco pollen render index.html.pm
 
 # Build JS and CSS
 yarn build
