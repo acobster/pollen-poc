dcba73bf2145d986b0d46451f3b065fef0a6a51f
Coby Tamayo <acobster@gmail.com>
2019-11-06 17:10:52 -0800

    index.html meta description

--- index.html.pm
+++ index.html.pm
@@ -1,6 +1,6 @@
 #lang pollen
 
-◊(define-meta description "My stuff")
+◊(define-meta description "Coby Tamayo's blog about code, and sometimes other things")
 ◊(define-meta publish-date "2016-09-26")
 
 ◊(blog-listing "./index.ptree")
